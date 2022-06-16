70c5941181353b3384257e9479e1dad7ac135641
96sunghyun <visioner2168@gmail.com>
2022-06-17 06:00:26 +0900

    메인페이지 css 변경

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,10 @@
-import React from 'react';
+import React from "react";
 import "./Main.css";
 import Web3 from "web3";
 import { NavLink } from "react-router-dom";
 import Header from "./Header";
 
 const Main = () => {
-  
-
   const connect = async () => {
     if (window.ethereum) {
       const web3 = new Web3(window.ethereum);
@@ -22,11 +20,10 @@ const Main = () => {
   return (
     <div className="Main">
       <Header />
-      <NavLink to="/" style={{ textDecoration: "none" }}>
-        <div className="connect" onClick={() => connect()}>
-          <h1>Connect your wallet!</h1>
-        </div>
-      </NavLink>
+
+      <div className="connect" onClick={() => connect()}>
+        <h1>Connect your wallet!</h1>
+      </div>
     </div>
   );
 };
